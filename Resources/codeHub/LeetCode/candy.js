dc67696e8918ab17ef8588422441b54df22ab5ac
Salishoma <githobbies100>
2020-02-03 16:00:40 +0100

    modified

---
+++ Resources/codeHub/LeetCode/candy.js
@@ -0,0 +1,33 @@
+/*
+There are N children standing in a line. Each child is assigned a rating value.
+
+You are giving candies to these children subjected to the following requirements:
+
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+What is the minimum candies you must give?
+
+LeetCode: 135. Candy
+*/
+
+function candy(ratings) {
+    let candies = Array(ratings.length).fill(1);
+    for (let i = 1; i < ratings.length; i++) {
+        if (ratings[i] > ratings[i - 1]) {
+            candies[i] = candies[i - 1] + 1;
+        }
+    }
+    let sum = candies[ratings.length - 1];
+    for (let i = ratings.length - 2; i >= 0; i--) {
+        if (ratings[i] > ratings[i + 1]) {
+            candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+        }
+        sum += candies[i];
+    }
+    return sum;
+}
+// candy([1,0,2])
+// candy([1,2,2])
+// candy([1,2,87,87,87,2,1])
+// candy([1,3,2,2,1])
+candy([1,3,4,5,2])
