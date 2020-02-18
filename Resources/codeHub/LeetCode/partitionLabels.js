89b5e6011a3516c3ec1613f40172f993901a35d9
Salishoma <githobbies100>
2020-02-18 02:01:21 +0100

    add

--- Resources/codeHub/LeetCode/partitionLabels.js
+++ Resources/codeHub/LeetCode/partitionLabels.js
@@ -1,3 +1,9 @@
+/*
+A string S of lowercase letters is given. We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
+
+LeetCode: 763. Partition Labels
+*/
+
 var partitionLabels = function(S) {
     let seen = {};
     let arr = [];
