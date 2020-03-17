698854eb9ea1fed552e5ca9e7fccf8d4b417baf9
Salishoma <githobbies100>
2020-03-17 03:41:02 +0100

    add

--- Resources/codeHub/LeetCode/smallestRepunitDivByK.js
+++ Resources/codeHub/LeetCode/smallestRepunitDivByK.js
@@ -1,10 +1,10 @@
 var smallestRepunitDivByK = function(K) {
     if(K === 1) return 1;
     if(K % 2 === 0) return -1;
-    let num = 1 % K;
-    for(let i = 1; i <= K; i++){
+    let num = 1;
+    for(let i = 2; i <= K; i++){
         num = (10 * num + 1) % K;
-        if(num === 0) return i + 1;
+        if(num === 0) return i;
     }
     return -1;
 };
