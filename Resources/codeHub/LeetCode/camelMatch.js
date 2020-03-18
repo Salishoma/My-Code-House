d6075bc63b982aec16dc15c85a95082f232e4b39
Salishoma <githobbies100>
2020-03-18 02:10:30 +0100

    add

--- Resources/codeHub/LeetCode/camelMatch.js
+++ Resources/codeHub/LeetCode/camelMatch.js
@@ -1,3 +1,11 @@
+/*
+A query word matches a given pattern if we can insert lowercase letters to the pattern word so that it equals the query. (We may insert each character at any position, and may insert 0 characters.)
+
+Given a list of queries, and a pattern, return an answer list of booleans, where answer[i] is true if and only if queries[i] matches the pattern.
+
+LeetCode: 1023. Camelcase Matching
+*/
+
 var camelMatch = function(queries, pattern) {
     let res = [];
     for(let q of queries){
