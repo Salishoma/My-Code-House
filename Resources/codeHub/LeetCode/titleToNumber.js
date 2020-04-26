bf4aabbd8b4eadff13179940edcd3e60c06f243d
Salishoma <githobbies100>
2020-04-26 22:19:59 +0100

    new

--- Resources/codeHub/LeetCode/titleToNumber.js
+++ Resources/codeHub/LeetCode/titleToNumber.js
@@ -1,3 +1,20 @@
+/*
+Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+For example:
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+
+LeetCode: 171. Excel Sheet Column Number
+*/
+
 var titleToNumber = function(s) {
     if(!s) return 0;
     let code = "A".charCodeAt(0) - 1;
