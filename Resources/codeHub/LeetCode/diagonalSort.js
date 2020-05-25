8733b08ba477284e407e274be8a2bff6fc433b3c
Salishoma <githobbies100>
2020-05-25 22:53:50 +0100

    add

--- Resources/codeHub/LeetCode/diagonalSort.js
+++ Resources/codeHub/LeetCode/diagonalSort.js
@@ -6,41 +6,28 @@ LeetCode: 1329. Sort the Matrix Diagonally
 
 var diagonalSort = function(mat) {
     for(let i = 0; i < mat[0].length; i++){
-        let r = 0;
-        let c = i;
-        let arr = [];
-        while(r < mat.length && c < mat[0].length){
-            arr.push(mat[r][c]);
-            r++;
-            c++;
-        }
-        arr.sort((a, b) => a - b);
-        r = 0;
-        c = i;
-        while(r < mat.length && c < mat[0].length){
-            mat[r][c] = arr[r]
-            r++;
-            c++;
-        }
+        diagonal(mat,0,i,0,i, true)
     }
     for(let i = 1; i < mat.length; i++){
-        let r = i;
-        let c = 0;
-        let arr = [];
-        while(r < mat.length && c < mat[0].length){
-            arr.push(mat[r][c]);
-            r++;
-            c++;
-        }
-        arr.sort((a, b) => a - b);
-        r = i;
-        c = 0;
-        while(r < mat.length && c < mat[0].length){
-            mat[r][c] = arr[c]
-            r++;
-            c++;
-        }
+        diagonal(mat,i,0,i,0,false)
     }
     return mat
 };
+const diagonal = (mat,r,c,i,j,side) => {
+    let arr = [];
+    while(r < mat.length && c < mat[0].length){
+        arr.push(mat[r][c]);
+        r++;
+        c++;
+    }
+    arr.sort((a, b) => a - b);
+    r = i;
+    c = j;
+    while(r < mat.length && c < mat[0].length){
+        mat[r][c] = side ? arr[r] : arr[c]
+        r++;
+        c++;
+    }
+    return mat;
+}
 diagonalSort([[3,3,1,1],[2,2,1,2],[1,1,1,2]])
