7f80ad92e313ad4a2f3a96d807cb8425066a2987
Salishoma <githobbies100>
2020-05-28 00:09:52 +0100

    add

---
+++ Resources/codeHub/LeetCode/shortestToChar2.js
@@ -0,0 +1,23 @@
+/*
+Given a string S and a character C, return an array of integers representing the shortest distance from the character C in the string.
+
+LeetCode: 821. Shortest Distance to a Character
+*/
+
+var shortestToChar = function(S, C) {
+    let prev = -Infinity
+    let indices = S.split('').map((el,i) => i).filter(i => S[i] ===C);
+    let res = [];
+    let j = 0
+    for(let i = 0; i < S.length; i++){
+        if(i > indices[j] && j < indices.length - 1){
+            prev = indices[j]
+            j++;
+        }
+        res[i] = Math.min(i - prev, indices[j] - i);
+        if(res[i] < 0) res[i] = Math.abs(res[i])
+    }     
+    return res;
+};
+shortestToChar("aaba","b")
+// shortestToChar("loveleetcode", 'e')
