f65966f0dda940e98ce13dde6b50173a69d020e2
Salishoma <githobbies100>
2020-07-05 22:15:53 +0100

    add

--- Resources/codeHub/LeetCode/isAdditiveNumber.js
+++ Resources/codeHub/LeetCode/isAdditiveNumber.js
@@ -15,13 +15,13 @@ var isAdditiveNumber = function(num) {
     let str = "";
     let seen = true;
     for(let i = num.length - 1; i > 1; i--){
-        str = num[i] + str;
+        str = `${num[i]}${str}`;
         if(num[i] === "0") continue;
         let s = str;
         let s2 = num[i - 1]
         for(let j = i - 2; j >= 0; j--){
             if(`${s2}`.startsWith("0") && s2.length > 1){
-                s2 = num[j] + s2  
+                s2 = `${num[j]}${s2}`  
                 seen = false;
             } else if(parseInt(s) >= parseInt(s2)){
                 let diff = s - s2;
@@ -33,7 +33,7 @@ var isAdditiveNumber = function(num) {
                     j -= ind;
                     seen = true;
                 }else {
-                    s2 = num[j] + s2;
+                    s2 = `${num[j]}${s2}`
                     seen = false;
                 }
             }else{
